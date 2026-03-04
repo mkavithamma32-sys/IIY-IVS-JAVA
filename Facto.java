c487b1a9a9090b411f5ce1f30965b11b37857899
mkavithamma32-sys <mkavithamma32@gmail.com>
2026-03-04 15:35:17 +0530

    Add factorial calculation in Facto.java

---
+++ Facto.java
@@ -0,0 +1,9 @@
+public class Facto {
+    static int  Fact(int n){
+        if(n==0) return 1;
+        return n * Fact(n-1);
+    }
+    public static void main(String[] args){
+        System.out.println(Fact(10));
+    }
+}
