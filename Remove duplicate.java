bc54c97ba17b3ac173b253ff1af2a4b4c34f5848
mkavithamma32-sys <mkavithamma32@gmail.com>
2026-03-05 10:28:28 +0530

    Implement RemoveDuplicates class to filter array

---
+++ Remove duplicate.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class RemoveDuplicates{
+
+  public static void main(String[] args){
+     int arr[] = { 5,2,8,2,5,1,3,8};
+
+ Arrays.sort(arr);
+
+int[] temp = new int[arr.length];
+int j=0;
+for(int i=0; i<arr.length; i++){
+	if(arr[i] != arr[i+1]){
+	  temp[j++] = arr[i];
+			}
+		}
+	temp[j++]= arr[arr.length-1];
+ 
+System.out.println("Array after removing duplicates:");
+for(int i=0;i<j;i++){
+  
+  System.out.println(temp[i]+ " ");
+
+  }
+}
+}
